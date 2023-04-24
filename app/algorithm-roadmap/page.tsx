2777692ad9296474a00525f9f4604a71f2b3bbcd
TProgrammer <TPoster@naver.com>
2023-04-24 12:00:06 +0900

    Removed AutoHegihtComponent

--- app/algorithm-roadmap/page.tsx
+++ app/algorithm-roadmap/page.tsx
@@ -1,17 +1,15 @@
-"use client"
-import AutoHeightImage from "@/components/shared/auto-height-image";
+import Image from "next/image";
 
 export default function AlgorithmRoadmap() {
 
     return(
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between xl:mx-auto">
-            {/* <Image
+            <Image
                 src="/TempRoadmap.png"
                 alt="임시 로드맵"
                 layout="fill"
                 className="flex items-center font-display text-2xl"
-            ></Image> */}
-            <AutoHeightImage src="/TempRoadmap.png" alt="임시이미지" />
+            ></Image>
         </div>
     );
 }
--- next.config.js
+++ next.config.js
@@ -17,9 +17,6 @@ const nextConfig = {
       },
     ];
   },
-  compiler: {
-    styledComponents: true,
-  },
 };
 
 module.exports = nextConfig;
