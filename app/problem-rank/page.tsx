a65b169e3f29fc22a5a0d252bcb76588d6e6b8a5
TProgrammer <TPoster@naver.com>
2023-05-02 14:40:48 +0900

    restore problem-rank/page.tsx

--- app/problem-rank/page.tsx
+++ app/problem-rank/page.tsx
@@ -1,17 +1,49 @@
-import CustomizedTables from "@/components/shared/MaterialTable";
+import Card from "@/components/home/card";
+import ComponentGrid from "@/components/home/component-grid";
 
 export default function ProblemRank() {
-  return(
-    <>
-      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-        <div className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
-            style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}>
 
-          {/* {features.map(({ title, description, demo, large }) => ( */}
-              <CustomizedTables />
-          {/* ))} */}
+    return(
+        <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
+        {features.map(({ title, description, demo, large }) => (
+            <Card
+                key={title}
+                title={title}
+                description={description}
+                demo={
+                    title === "Beautiful, reusable components" ? (
+                    <ComponentGrid />
+                    ) : (
+                    demo
+                    )
+                }
+                large={large}
+            />
+        ))}
         </div>
+    );
+}
+
+const features = [
+  {
+    title: "Beautiful, reusable components",
+    description:
+      "Pre-built beautiful, a11y-first components, powered by [Tailwind CSS](https://tailwindcss.com/), [Radix UI](https://www.radix-ui.com/), and [Framer Motion](https://framer.com/motion)",
+    large: true,
+  },
+  {
+    title: "Hooks, utilities, and more",
+    description:
+      "Precedent offers a collection of hooks, utilities, and `@vercel/og`",
+    demo: (
+      <div className="grid grid-flow-col grid-rows-3 gap-10 p-10">
+        <span className="font-mono font-semibold">useIntersectionObserver</span>
+        <span className="font-mono font-semibold">useLocalStorage</span>
+        <span className="font-mono font-semibold">useScroll</span>
+        <span className="font-mono font-semibold">nFormatter</span>
+        <span className="font-mono font-semibold">capitalize</span>
+        <span className="font-mono font-semibold">truncate</span>
       </div>
-    </>
-  );
-};
+    ),
+  },
+];
